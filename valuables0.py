70454bee4ce28577e205a0ffba5773c817b479f9
杉　健太郎 <onlyforios2015@gmail.com>
2025-01-16 21:07:37 +0900

    Update valuables0.py

--- valuables0.py
+++ valuables0.py
@@ -1,5 +1,8 @@
 # 学習実験用変数
 
+from main0 import EnvironmentInfo, env_info
+
+
 EPISODES = 500         # エピソード数
 BATCH_SIZE = 256        # バッチサイズ
 
@@ -10,7 +13,7 @@ EPSILON_MIN = 0.01      # 最小探索率
 EPSILON_DECAY = 0.997   # エピソードごとの探索率の減衰率
 LERNING_RATE = 0.002    # 学習率
 
-C_W = 5000000                 # 強制終了時のペナルティ
+C_W = 500000                 # 強制終了時のペナルティ
 
 # 保存先
 DIR = "./results/"
@@ -18,3 +21,5 @@ DIR = "./results/"
 # IPソケット通信接続先
 IP = '127.0.0.1'
 PORT = 1234
+# 変数の初期化
+NowEnv = EnvironmentInfo(env_info)
